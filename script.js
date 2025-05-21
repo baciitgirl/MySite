3596a84600c179fa2a00fe1d5dedf515c5b37630
baciitgirl <anna.bacanau@gmail.com>
2025-05-21 10:49:49 +0200

    JS File changed

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ document.querySelectorAll(".nav-link").forEach(link => {
   function generatePDF() {
     const element = document.getElementById('lebenslauf-inhalt');
     const options = {
-      margin:       0.5,
-      filename:     'lebenslauf-anna-bacanau.pdf',
-      image:        { type: 'jpeg', quality: 0.98 },
-      html2canvas:  { scale: 2 },
-      jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
+      margin: 0.5,
+      filename: 'lebenslauf-anna-bacanau.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2, scrollY: 0 },
+      jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' },
+      pagebreak: { mode: ['avoid-all', 'css', 'legacy'] }
     };
     html2pdf().set(options).from(element).save();
   }
-
