f54d93e8e505d53a689d26010e59456f7117eb6f
baciitgirl <anna.bacanau@gmail.com>
2025-05-21 10:36:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const currentPath = window.location.pathname.split("/").pop();
+const element = document.getElementById('lebenslauf-inhalt');
+
+document.querySelectorAll(".nav-link").forEach(link => {
+  if (link.getAttribute("href") === currentPath) {
+    link.classList.add("active");
+  } else {
+    link.classList.remove("active");
+  }
+});
+
+
+
+
+  function generatePDF() {
+    const element = document.getElementById('lebenslauf-inhalt');
+    const options = {
+      margin:       0.5,
+      filename:     'lebenslauf-anna-bacanau.pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 2 },
+      jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
+    };
+    html2pdf().set(options).from(element).save();
+  }
+
